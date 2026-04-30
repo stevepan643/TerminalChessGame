3416f727543eba8f3dfcfa3af36a80437cd5368c
stevepan643 <steve.pan.mail@gmail.com>
2026-04-30 21:59:17 +0800

    WIP: UI system 2026043001

--- tui.h
+++ tui.h
@@ -32,4 +32,64 @@ void draw_frame();
 void disable_rew_mode();
 void enable_raw_mode();
 
+typedef struct {
+	size_t x, y;
+	size_t w, h;
+} Rect;
+
+typedef uint32_t TypeID;
+
+typedef struct {
+	TypeID id;
+	TypeID parent;
+	const char *name;
+} TypeInfo;
+
+TypeID type_register(const char *name, TypeID parent);
+
+typedef struct Widget	Widget;
+typedef struct Panel	Panel;
+typedef struct Button	Button;
+typedef struct Text	Text;
+typedef struct ProgBar	ProgBar;
+typedef struct Input	Input;
+
+typedef struct {
+	void (*draw)(Widget *self);
+	void (*update)(Widget *self, float dt);
+	void (*handle_input)(Widget *self, int key);
+	void (*destroy)(Widget *self);
+} WidgetVTable;
+
+struct Widget {
+	TypeID type;
+	Rect rect;
+	WidgetVTable *vtable;
+};
+
+typedef struct Panel {
+	Widget base;
+
+	Widget **children;
+	size_t count;
+} Panel;
+
+Widget *widget_new(TypeID type);
+void widget_draw(Widget *w);
+void widget_update(Widget *w, float dt);
+void widget_handle_input(Widget *w, int key);
+void widget_destroy(Widget *w);
+
+Widget *panel_new();
+void panel_add(Panel *p, Widget *w, Rect r);
+
+Widget *button_new(const char32_t *str, Color t, Color h, Color p);
+
+Widget *text_new(const char32_t *str, Color t, Color b);
+
+Widget *progbar_new(Color f, Color u);
+void progbar_set_progress(ProgBar *pb, float p);
+
+Widget *input_new(const char32_t *d);
+
 #endif
